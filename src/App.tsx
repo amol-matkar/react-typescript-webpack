674ebec14f6102468b636a064ea0f8b02d919622
Amol Matkar <amatkarsmailbox@gmail.com>
2023-04-06 20:08:41 +0530

    Second push with working react app with webpack

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './styles/style.css'
+import reactImage from './react.png'
+
+export default function App() {
+    return (
+        <div>
+            <h1>App</h1>
+            <img src={reactImage} alt="React" />
+        </div>
+    )
+}
