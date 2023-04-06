bbe9243a0a4a71453629af9bed29d479123c0113
Amol Matkar <amatkarsmailbox@gmail.com>
2023-04-06 21:01:02 +0530

    css chnages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-import './styles/style.css'
+import style from './App.module.css';
 import reactImage from './react.png'
 
 export default function App() {
     return (
-        <div>
+        <div className={style.app}>
             <h1>App</h1>
             <img src={reactImage} alt="React" />
         </div>
